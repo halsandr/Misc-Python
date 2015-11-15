3430fc778bced304f7bd18da3e3aa84775be2069
Andy <halsandr@hotmail.co.uk>
2015-11-15 22:07:38 +0000

    Update Tec_md5_crack_par.py

--- Tec_md5_crack_par.py
+++ Tec_md5_crack_par.py
@@ -2,7 +2,7 @@
 
 import hashlib, os, time, math
 from hashlib import md5
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Manager
 
 def screen_clear(): # Small function for clearing the screen on Unix or Windows
 	if os.name == 'nt':
@@ -25,8 +25,9 @@ print ""
 hash = raw_input("Hash: ")
 print ""
 file = raw_input("Wordlist: ")
+
 screen_clear()
-print "Cracking the password for \"" + user + "\" using " 
+
 time1 = time.time() # Begins the 'Clock' for timing
 
 realm = "Technicolor Gateway" # These 3 variables dont appear to change
@@ -39,24 +40,22 @@ file = open(file, 'r') # Opens the wordlist file
 wordlist = file.readlines() # This enables us to use len()
 length = len(wordlist)
 
-screen_clear()
 print "Cracking the password for \"" + user + "\" using " + str(length) + " words"
 
 break_points = []  # List that will have start and stopping points
 for i in range(cores):  # Creates start and stopping points based on length of word list
     break_points.append({"start":int(math.ceil((length+0.0)/cores * i)), "stop":int(math.ceil((length+0.0)/cores * (i + 1)))})
 
-solved = False
+finished = False
 
-def solver(state):
-	if state == True:
-		p.terminate
-		p.join
-		global solved
-		solved = state
+def finisher(answer):
+	if answer:
+		global finished
+		finished = True
+		p.terminate()
 
 def pwd_find(start, stop):
-    for number in range(start, stop):
+	for number in range(start, stop):
 		word = (wordlist[number])
 		pwd = word.replace("\n","") # Removes newline character
 		HA1 = md5(user + ":" + realm + ":" + pwd).hexdigest()
@@ -68,27 +67,25 @@ def pwd_find(start, stop):
 			print "\"" + pwd + "\"" + " = " + hidepw + " (in " + str(timetotal) + " seconds)"
 			print ""
 			return True
+	if True:
+		return False
 
-if __name__ == '__main__':  # Added this because the multiprocessor module acts funny without it.
+if __name__ == '__main__':  # Added this because the multiprocessor module sometimes acts funny without it.
 
     p = Pool(cores)  # Number of processes to create.
     for i in break_points:  # Cycles though the breakpoints list created above.
-        a = p.apply_async(pwd_find, kwds=i, args=tuple(), callback=solver)  # This will start the separate processes.
+        a = p.apply_async(pwd_find, kwds=i, args=tuple(), callback=finisher)  # This will start the separate processes.
     p.close() # Prevents any more processes being started
     p.join() # Waits for worker process to end
 
-if solved == True:
-	end = raw_input("hit enter to exit")
-	file.close() # Closes the wordlist file
-	screen_clear()
-	exit()
-else:
-	screen_clear()
+if finished == False:
 	time2 = time.time() # Stops the 'Clock'
 	totaltime = math.ceil(time2 - time1) # Calculates the time taken
+	screen_clear()
 	print "Sorry your password was not found (in " + str(totaltime) + " seconds) out of " + str(length) + " words"
 	print ""
-	end = raw_input("hit enter to exit")
-	file.close() # Closes the wordlist file
-	screen_clear()
-	exit()
+
+end = raw_input("Hit enter to exit")
+file.close() # Closes the wordlist file
+screen_clear()
+exit()
